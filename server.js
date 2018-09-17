78317357ff80bbb680bde3be19fc1e81cb7262c8
natthawarapon <s5930213012@phuket.psu.ac.th>
2018-09-17 16:39:01 +0700

    ใส่รายละเอียดในหน้าต่างๆ

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs');
+
+app.get('/', function (req, res) {
+    res.render('pages/home');
+});
 app.get('/home', function (req, res) {
     res.render('pages/home');
 });
+app.get('/student', function (req, res) {
+    res.render('pages/student');
+});
+app.get('/subjects', function (req, res) {
+    res.render('pages/subjects');
+});
+
 console.log('App is running at http://localhost:8080');
 app.listen(8080);
-
