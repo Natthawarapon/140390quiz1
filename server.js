8fd83bbc47aa28dd08d252d0f37ce2f44536f075
natthawarapon <s5930213012@phuket.psu.ac.th>
2018-09-17 17:36:49 +0700

    ดึงข้อมูลจากdatabase

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var app = express();
 
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host     : 'www.db4free.net',
-  user     : 's140390',
-  password : 'abc123**',
-  database : 'db140390'
-});
+    host     : 'www.db4free.net',
+    user     : 's140390',
+    password : 'abc123**',
+    database : 'db140390'
+  });
+  connection.connect();
 
-connection.connect();
 ////////////////////////////////////
 app.set('view engine','ejs');
 
@@ -21,10 +21,27 @@ app.get('/home', function (req, res) {
     res.render('pages/home');
 });
 app.get('/student', function (req, res) {
-    res.render('pages/student');
+    
+ 
+    connection.query('select * from students', function (err, rows, fields) {
+        if (err) throw err
+        res.render('pages/student',{student:rows})
+        console.log('The solution is: ', rows[0].solution)
+      })
+      
+     // connection.end()
 });
 app.get('/subjects', function (req, res) {
-    res.render('pages/subjects');
+ 
+ 
+    connection.query('select * from subjects', function (err, rows, fields) {
+        if (err) throw err
+        
+        res.render('pages/subjects',{subjects:rows})
+        console.log('The solution is: ', rows[0].solution)
+      })
+      
+      //connection.end()
 });
 
 console.log('App is running at http://localhost:8080');
